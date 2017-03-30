387c6a3e2aa9f11b32430ae4c5c0f74827c31fc8
Shalmi Patel <patels19@mcmaster.ca>
2017-03-30 00:41:25 -0400

    for di

--- ClaimMyBag/cas2xb3_finalProject/src/cas2xb3_finalprototype/ourApplication.java
+++ ClaimMyBag/cas2xb3_finalProject/src/cas2xb3_finalprototype/ourApplication.java
@@ -9,10 +9,17 @@ import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.JToggleButton;
+import javax.swing.JButton;
 
 public class ourApplication {
 
 	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
 
 	/**
 	 * Launch the application.
@@ -42,12 +49,61 @@ public class ourApplication {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 777, 609);
+		frame.setBounds(100, 100, 623, 534);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("Airline");
 		lblNewLabel.setBounds(61, 55, 69, 43);
 		frame.getContentPane().add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(138, 63, 146, 26);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("Claim Number");
+		lblNewLabel_1.setBounds(15, 125, 115, 20);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(138, 122, 146, 26);
+		frame.getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblNewLabel_2 = new JLabel("Incident Date");
+		lblNewLabel_2.setBounds(15, 184, 115, 20);
+		frame.getContentPane().add(lblNewLabel_2);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(138, 181, 146, 26);
+		frame.getContentPane().add(textField_2);
+		textField_2.setColumns(10);
+		
+		textField_3 = new JTextField();
+		textField_3.setBounds(138, 235, 146, 26);
+		frame.getContentPane().add(textField_3);
+		textField_3.setColumns(10);
+		
+		JLabel lblNewLabel_3 = new JLabel("Claim Type");
+		lblNewLabel_3.setBounds(30, 238, 115, 20);
+		frame.getContentPane().add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_4 = new JLabel("Item");
+		lblNewLabel_4.setBounds(76, 295, 69, 20);
+		frame.getContentPane().add(lblNewLabel_4);
+		
+		textField_4 = new JTextField();
+		textField_4.setBounds(138, 292, 146, 26);
+		frame.getContentPane().add(textField_4);
+		textField_4.setColumns(10);
+		
+		JLabel lblNewLabel_5 = new JLabel("New label");
+		lblNewLabel_5.setBounds(15, 16, 69, 20);
+		frame.getContentPane().add(lblNewLabel_5);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(177, 334, 115, 29);
+		frame.getContentPane().add(btnNewButton);
 	}
 }
