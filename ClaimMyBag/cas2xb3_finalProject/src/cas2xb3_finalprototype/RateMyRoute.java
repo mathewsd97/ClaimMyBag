4e2eb3be80dfd5037428c66bdb6c37394efcba31
Shalmi Patel <patels19@mcmaster.ca>
2017-04-03 15:31:30 -0400

    boujour

--- ClaimMyBag/cas2xb3_finalProject/src/cas2xb3_finalprototype/RateMyRoute.java
+++ ClaimMyBag/cas2xb3_finalProject/src/cas2xb3_finalprototype/RateMyRoute.java
@@ -10,12 +10,15 @@ import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class RateMyRoute extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField textField;
 	private JTextField textField_1;
+	private JTextField textField_2;
 
 	/**
 	 * Launch the application.
@@ -38,7 +41,7 @@ public class RateMyRoute extends JFrame {
 	 */
 	public RateMyRoute() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 486, 426);
+		setBounds(100, 100, 486, 659);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -50,11 +53,11 @@ public class RateMyRoute extends JFrame {
 		contentPane.add(lblRateMyRoute);
 		
 		JLabel lblStart = new JLabel("Departure");
-		lblStart.setBounds(22, 83, 49, 14);
+		lblStart.setBounds(22, 83, 83, 14);
 		contentPane.add(lblStart);
 		
 		JLabel lblArrival = new JLabel("Arrival");
-		lblArrival.setBounds(40, 135, 31, 14);
+		lblArrival.setBounds(40, 135, 65, 14);
 		contentPane.add(lblArrival);
 		
 		textField = new JTextField();
@@ -67,13 +70,35 @@ public class RateMyRoute extends JFrame {
 		textField_1.setBounds(120, 132, 211, 20);
 		contentPane.add(textField_1);
 		
+		JLabel lblMonth = new JLabel("Month");
+		lblMonth.setBounds(40, 179, 69, 20);
+		contentPane.add(lblMonth);
+		
 		JButton btnEnter = new JButton("Enter");
-		btnEnter.setBounds(214, 177, 146, 53);
+		btnEnter.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String Start = lblStart.getText();
+				String End = lblArrival.getText();
+				int Month = Integer.parseInt(lblMonth.getText());
+				
+				ArrayList<String> Finalreault = RateMyRouteFinal.totalOutput(Start, End, Month);
+				
+				
+				
+			}
+		});
+		btnEnter.setBounds(208, 234, 146, 53);
 		contentPane.add(btnEnter);
 		
 		JButton button = new JButton("Home");
-		button.setBounds(58, 177, 146, 53);
+		button.setBounds(60, 234, 146, 53);
 		contentPane.add(button);
-	}
+		
 
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(120, 176, 211, 26);
+		contentPane.add(textField_2);
+		textField_2.setColumns(10);
+	}
 }
