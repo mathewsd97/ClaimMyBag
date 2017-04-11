fc308c6cf7f172e89bdcc1fc8c3b6f4f72e1b131
willis12 <willis12@mcmaster.ca>
2017-04-11 14:42:39 -0400

    cleaned up code

--- ClaimMyBag/cas2xb3_finalProject/src/cas2xb3_finalprototype/NewSearch.java
+++ ClaimMyBag/cas2xb3_finalProject/src/cas2xb3_finalprototype/NewSearch.java
@@ -5,6 +5,7 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import java.awt.Font;
@@ -14,14 +15,16 @@ import javax.swing.AbstractAction;
 import java.awt.event.ActionEvent;
 import javax.swing.Action;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class NewSearch extends JFrame {
 
 	private JPanel contentPane;
 	private JPanel rateroute;
 	private JTextField textField_1;
-	//private final Action action = new SwingAction();
-	private int results;
+	private LuggageT resultingLuggage;
+	// private final Action action = new SwingAction();
+	private String results;
 	private String statement;
 	private JTextField textField;
 
@@ -45,80 +48,98 @@ public class NewSearch extends JFrame {
 	 * Create the frame.
 	 */
 	public NewSearch() {
+		// newsearch
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 488, 390);
+		setBounds(100, 100, 431, 380);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JLabel lblNewSearch = new JLabel("New Search");
 		lblNewSearch.setFont(new Font("Tahoma", Font.BOLD, 20));
-		lblNewSearch.setBounds(136, 11, 133, 39);
+		lblNewSearch.setBounds(146, 31, 121, 25);
 		contentPane.add(lblNewSearch);
-		
+
 		JLabel lblNewLabel = new JLabel("Claim Number");
-		lblNewLabel.setBounds(25, 84, 124, 14);
+		lblNewLabel.setBounds(39, 75, 102, 20);
 		contentPane.add(lblNewLabel);
-		
+
 		textField_1 = new JTextField();
 		textField_1.setColumns(10);
-		textField_1.setBounds(146, 78, 230, 26);
+		textField_1.setBounds(156, 72, 224, 26);
 		contentPane.add(textField_1);
-		
+
 		JButton btnNewButton = new JButton("Enter");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				
 				try {
 					FileReading.luggage();
 				} catch (Exception e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
-				//String airline = lblNewLabel.getText();
-				String ClaimNumber = lblNewLabel.getText();
-				results = Search.indexOf(FileReading.totalLuggage, ClaimNumber);
-				if(Search.indexOf(FileReading.totalLuggage, ClaimNumber) == -1 ){
-					statement = "Luggage not found.";
-					textField.setText(statement);
-				}else{
-					statement = "Luggage Found.";
-					textField.setText(statement);
+				// String airline = lblNewLabel.getText();
+				String ClaimNumber = textField_1.getText();
+				Search test = new Search();
+				try {
+					resultingLuggage = Search.getLuggage(FileReading.luggage(),ClaimNumber);
+				//	results = FileReading.totalLuggage.get(0).getAirlineName();
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
 				}
+				try {
+					if (resultingLuggage == null) {
+						statement = "Luggage not found.";
+						textField.setText(statement);
+					} else if (resultingLuggage.getKnownAirportName().equals("")){
+						textField.setText("Luggage found, but no locational info could be uncovered!");
+					}else {
+						statement = "Luggage Found at ";
+						results = resultingLuggage.getKnownAirportName();
+						textField.setText(statement + results);
+					}
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
 				}
+			}
 		});
-		//btnNewButton.setAction(action);
-		btnNewButton.setBounds(223, 126, 153, 49);
+
+		// btnNewButton.setAction(action);
+		btnNewButton.setBounds(232, 123, 148, 67);
 		contentPane.add(btnNewButton);
-		
 
 		JButton btnHome = new JButton("Rate My Route");
 		btnHome.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				RateMyRoute rate = new RateMyRoute();
-				rate.setVisible(true);
-				contentPane.setVisible(false);
-				
+				 RateMyRoute rate = new RateMyRoute();
+				 rate.setVisible(true);
+
 			}
 		});
-		btnHome.setBounds(83, 126, 133, 49);
+		btnHome.setBounds(38, 123, 179, 67);
 		contentPane.add(btnHome);
-		
+
 		textField = new JTextField();
-		textField.setBounds(83, 234, 293, 49);
+		textField.setBounds(39, 221, 341, 87);
 		contentPane.add(textField);
 		textField.setColumns(10);
-		
+
 		JLabel lblResults = new JLabel("Results:");
-		lblResults.setBounds(83, 212, 69, 20);
+		lblResults.setBounds(39, 194, 56, 20);
 		contentPane.add(lblResults);
 
 	}
+
 	private class SwingAction extends AbstractAction {
 		public SwingAction() {
 			putValue(NAME, "SwingAction");
 			putValue(SHORT_DESCRIPTION, "Some short description");
 		}
+
 		public void actionPerformed(ActionEvent e) {
 		}
 	}
--- ClaimMyBag/cas2xb3_finalProject/src/cas2xb3_finalprototype/Search.java
+++ ClaimMyBag/cas2xb3_finalProject/src/cas2xb3_finalprototype/Search.java
@@ -4,46 +4,50 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Search {
-/**
- * This class should not be instantiated.
- */
-private Search() { }
+	/**
+	 * This class should not be instantiated.
+	 */
+	public Search() { }
 
-/**
- * Returns the index of the specified key in the specified array.
- *
- */
-public static int indexOf(ArrayList<LuggageT> a, String key) {
-    int lo = 0;
-    int hi = a.size() - 1;
-    while (lo <= hi) {
-        // Key is in a[lo..hi] or not present.
-        int mid = lo + (hi - lo) / 2;
-        if      (key.compareTo(a.get(mid).getClaimNumber()) < 0) hi = mid - 1;
-        else if (key.compareTo(a.get(mid).getClaimNumber()) > 0) lo = mid + 1;
-        else return mid;
-    }
-    return -1;
-}
+	/**
+	 * Returns the index of the specified key in the specified array.
+	 *
+	 */
+	public static int indexOf(ArrayList<LuggageT> a, String key) {
+		int lo = 0;
+		int hi = a.size() - 1;
+		while (lo <= hi) {
+			// Key is in a[lo..hi] or not present.
+			int mid = lo + (hi - lo) / 2;
+			if      (key.compareTo(a.get(mid).getClaimNumber()) < 0) hi = mid - 1;
+			else if (key.compareTo(a.get(mid).getClaimNumber()) > 0) lo = mid + 1;
+			else return mid;
+		}
+		return -1;
+	}
 
-/**
- * Returns the index of the specified key in the specified array.
- *
- */
-@Deprecated
-public static int rank(String key, ArrayList<LuggageT> a) {
-    return indexOf(a, key);
-}
+	/**
+	 * Returns the index of the specified key in the specified array.
+	 *
+	 */
+	@Deprecated
+	public static int rank(String key, ArrayList<LuggageT> a) {
+		return indexOf(a, key);
+	}
 
-public LuggageT getLuggage(String claim){
-	ArrayList<LuggageT> input = new ArrayList<LuggageT>();
-    FileReading a = new FileReading();
- 	input = a.luggage();
- 	Merge test = new Merge(input);
-    test.sort();
-    String claimNumber = claim;
-    return input.get(Search.indexOf(input, claim));
-}
+	public static LuggageT getLuggage(ArrayList<LuggageT> list, String claim) throws Exception{
+		LuggageT luggage = null;
+		Merge test = new Merge(list);
+		test.sort();
+		String claimNumber = claim;
+		if(Search.indexOf(list, claim) == -1){
+			luggage = null;
+		}else{
+		luggage = list.get(Search.indexOf(list, claim));
+		}
+		return luggage;
+
+	}
 
 }
 
